b2bc6223bc2ec03d333de27bc52ba9bb25ae89d5
beanstown106 <nbenis106@gmail.com>
2017-01-04 13:29:17 -0500

    Pixel Navbar animation toggle [1/2] *made to work with the exposure for themers to disable navbar animation
    Change-Id: I6a98920cb83894097db7b51dd46355607a0e03d8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3595,6 +3595,12 @@ public final class Settings {
         };
 
         /**
+         * Whether to enable the pixel navbar animation
+         * @hide
+         */
+        public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
